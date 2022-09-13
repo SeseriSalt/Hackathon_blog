b7df4164279ddc59b1d51c5945db8d816fbb57a4
daiki.fujioka <daiki.fujioka@techouse.jp>
2022-09-13 07:06:45 +0000

    blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,34 @@
 class BlogsController < ApplicationController
   def show
+    @blog = Blog.find(params[:id])
   end
+
+  def new
+    @blog = current_user.blogs.build
+  end
+
+  def edit
   
+  end
+
   def create
+    @blog = current_user.blogs.build(blog_params)
+    if @blog.save
+      redirect_to root_path
+    else
+      render 'blogs/new'
+    end
   end
-  
+
   def update
   end
-  
-  def delete
+
+  def destroy
   end
+  
+  private
+
+    def blog_params
+      params.require(:blog).permit(:content)
+    end
 end
