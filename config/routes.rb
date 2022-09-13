bffb9c62419b37526cdbb41384c7761308faa558
Yada <sho8.kthy@gmail.com>
2022-09-13 04:56:09 +0000

    userのviewとcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,16 @@ Rails.application.routes.draw do
   resources 'blogs'
   resources 'mypages'
   # r 'application#hello'
-end
+  
+  
+  devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
