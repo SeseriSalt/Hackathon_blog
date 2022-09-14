044b6c7f5fd749447483f2bf14740d0c22e15689
Ubuntu <ubuntu@ip-172-31-2-145.ap-northeast-1.compute.internal>
2022-09-14 06:07:42 +0000

    投稿時のtagの修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -41,6 +41,6 @@ class BlogsController < ApplicationController
   private
 
     def blog_params
-      params.require(:blog).permit(:title, :content)
+      params.require(:blog).permit(:title, :tag, :content)
     end
 end
